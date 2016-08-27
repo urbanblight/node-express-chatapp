1f2d45420b8f09fba525fdbbf6d28a2635604aeb
Matt Urban <matt.urban@adops.com>
2016-08-27 13:40:21 -0700

    Parsing Form Data From The Request Body
    * Intros `body-parser` npm package

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 var express = require('express');
 var app = express();
 var rooms = require("./data/rooms.json");
+var bodyParser = require("body-parser");
 
 app.set('view engine', 'pug');
 
 app.use(express.static("public"));
 app.use(express.static("node_modules/bootstrap/dist"));
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.get('/', function(req, res){
 	res.render('index', {title: "Home"});
@@ -17,6 +19,10 @@ app.get('/admin/rooms', function(req, res){
 
 app.get('/admin/rooms/add', function(req, res){
 	res.render('add');
+});
+
+app.post('/admin/rooms/add', function(req, res){
+	console.log(req.body);
 });
 
 app.listen(3000, function() {
