7495eca7ae6731ac098d3bebb0be8938ba0a1018
Matt Urban <matt.urban@adops.com>
2016-09-15 20:56:25 -0700

    API Get Room Messages
    * intros room message endpoints
    * intros message data file

--- api.js
+++ api.js
@@ -2,9 +2,27 @@ var express = require("express");
 router = express.Router();
 module.exports = router;
 
+_ = require("lodash");
+
 var rooms = require("./data/rooms.json");
+var messages = require("./data/messages.json");
 
 router.route('/rooms')
 	.get(function(req, res){
 		res.json(rooms);	
+	});
+
+router.route('/rooms/:roomId/messages')
+	.get(function(req, res){
+		var roomId = req.params.roomId;
+		var roomMessages = messages.filter(m => m.roomId == roomId);
+		var room = _.find(rooms, r => r.id === roomId);
+		if(!room){
+			next(new Error("Room not found!"));
+			return;
+		}
+		res.json({
+			room: room,
+			messages: roomMessages
+		});
 	});
